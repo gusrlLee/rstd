7d5e03fc2eb72deffb663f22e577e0928a2faace
gusrlLee <gusrl3204@naver.com>
2025-10-16 23:54:38 +0900

    restart we need to save png file

--- main.cu
+++ main.cu
@@ -1,7 +1,56 @@
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb/stb_image_write.h>
+
 #include <iostream>
+#include <cuda_runtime.h>
+
+#define CUDA_CHECK(val) check_cuda((val), #val, __FILE__, __LINE__)
+void check_cuda(cudaError_t result, char const *const func, const char *const file, int const line)
+{
+    if (result)
+    {
+        std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at " << file << ":" << line << " '" << func << "' \n";
+        // Make sure we call CUDA Device Reset before exiting
+        cudaDeviceReset();
+        exit(99);
+    }
+}
+
+__global__ void render(float *fb, int max_x, int max_y)
+{
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if ((i >= max_x) || (j >= max_y))
+        return;
+    int pixel_index = j * max_x * 3 + i * 3;
+    fb[pixel_index + 0] = float(i) / max_x;
+    fb[pixel_index + 1] = float(j) / max_y;
+    fb[pixel_index + 2] = 0.2;
+}
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    int w = 1280;
+    int h = 720;
+
+    int tx = 8;
+    int ty = 8;
+
+    std::cout << "Rendering a " << w << "x" << h << " image ";
+    std::cout << "in " << tx << "x" << ty << " blocks.\n";
+
+    float *fb;
+    checkCudaErrors(cudaMallocManaged((void **)&fb, fb_size));
+    int num_px = w * h;
+    // Render our buffer
+    dim3 blocks(w/tx+1,h/ty+1);
+    dim3 threads(tx,ty);
+    render<<<blocks, threads>>>(fb, nx, ny);
+    CUDA_CHECK(cudaGetLastError());
+    CUDA_CHECK(cudaDeviceSynchronize());
+
+    
+
+
     return 0;
 }
